0b90cc2aa46ffe44dec84eafd6f94599d68fdebf
Jorge Vega (GLOBANT) <jorgevega.globant@latam.com>
2026-04-10 23:29:56 -0400

    cambios sh

--- git_sync.sh
+++ git_sync.sh
@@ -3,14 +3,14 @@
 # Navegar a la carpeta del repositorio Git
 cd "$(dirname "$0")/bencinachile"
 
-echo "📍 Procesando rama feature/ia_help..."
+echo "📍 Procesando rama feature/ia_help2..."
 git add .
 git commit -m "cambios sh"
-git push origin feature/ia_help
+git push origin feature/ia_help2
 
 echo "📍 Haciendo merge a develop..."
 git checkout develop
-git merge feature/ia_help
+git merge feature/ia_help2
 git push origin develop
 
 echo "📍 Haciendo merge a master..."
